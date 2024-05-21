162b97fb81876a901e96d58a418c11bae4a01383
syedumarhusain <syedumarhusain1@gmail.com>
2024-05-21 15:26:59 +0530

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import './App.css';
+import './App.scss';
+import { Route, Routes } from 'react-router-dom'
+// import Home from './components/Home'
+// import About from './components/About'
+// import Contact from './components/Contact'
+import Layout from './components/Layout'
+// import Portfolio from './components/Portfolio'
+// import Dashboard from './components/Dashboard'
 
 
-const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-};
 
 function App() {
   return (
     <>
-    <h1>{user.name}</h1>
-    <img
-      className="avatar"
-      src={user.imageUrl}
-      alt={'Photo of ' + user.name}
-      style={{
-        width: user.imageSize,
-        height: user.imageSize
-      }}
-    />
-  </>
-  );
+      <Routes>
+      <Route path="/" element={<Layout />}>
+      {/* <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Route>
+      </Routes>
+    </>
+  )
 }
 
 export default App;
